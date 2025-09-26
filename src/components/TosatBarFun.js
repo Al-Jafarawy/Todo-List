8f0f533c8ba7245260eb2cc4d17563878904cf50
Al-Jafarawy <prog.al.jafarawy@gmail.com>
2025-09-26 08:23:21 +0300

    Edit tosatBarfun

--- src/components/TosatBarFun.js
+++ src/components/TosatBarFun.js
@@ -1,4 +1,3 @@
-import * as React from "react";
 import Button from "@mui/material/Button";
 import Snackbar from "@mui/material/Snackbar";
 import IconButton from "@mui/material/IconButton";
@@ -6,30 +5,38 @@ import CloseIcon from "@mui/icons-material/Close";
 import Alert from "@mui/material/Alert";
 import Stack from "@mui/material/Stack";
 
-export default function TosatBarFun({ open ,content }) {
+export default function TosatBarFun({ open, content, onClose }) {
   const action = (
     <>
       <Button color="secondary" size="small">
         UNDO
       </Button>
-      <IconButton size="small" aria-label="close" color="inherit">
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={onClose}
+      >
         <CloseIcon fontSize="small" />
       </IconButton>
     </>
-  )
+  );
 
   return (
     <div>
       <Stack sx={{ width: "100%" }} spacing={2}>
-        
         <Snackbar
           open={open}
-          
           autoHideDuration={1000}
-          message="Note archived"
+          onClose={onClose}
           action={action}
         >
-          <Alert variant="filled" severity="success">
+          <Alert
+            onClose={onClose}
+            severity="success"
+            variant="filled"
+            sx={{ width: "100%" }}
+          >
             {content}
           </Alert>
         </Snackbar>
