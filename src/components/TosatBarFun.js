38f18d8473ec8dd99c4295e2d50de9d8ad620510
Al-Jafarawy <prog.al.jafarawy@gmail.com>
2025-09-27 08:27:00 +0300

    handel unused code in tosatBarFn

--- src/components/TosatBarFun.js
+++ src/components/TosatBarFun.js
@@ -1,36 +1,12 @@
-import Button from "@mui/material/Button";
 import Snackbar from "@mui/material/Snackbar";
-import IconButton from "@mui/material/IconButton";
-import CloseIcon from "@mui/icons-material/Close";
 import Alert from "@mui/material/Alert";
 import Stack from "@mui/material/Stack";
 
 export default function TosatBarFun({ open, content, onClose }) {
-  const action = (
-    <>
-      <Button color="secondary" size="small">
-        UNDO
-      </Button>
-      <IconButton
-        size="small"
-        aria-label="close"
-        color="inherit"
-        onClick={onClose}
-      >
-        <CloseIcon fontSize="small" />
-      </IconButton>
-    </>
-  );
-
   return (
     <div>
       <Stack sx={{ width: "100%" }} spacing={2}>
-        <Snackbar
-          open={open}
-          autoHideDuration={1000}
-          onClose={onClose}
-          action={action}
-        >
+        <Snackbar open={open} autoHideDuration={1000} onClose={onClose}>
           <Alert
             onClose={onClose}
             severity="success"
